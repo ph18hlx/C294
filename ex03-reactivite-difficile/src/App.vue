56459a3ad7bf85a4c7f36ae0652f95e844e46cc2
Thibaud Blasco Dias <ph18hlx@eduvaud.ch>
2026-08-28 12:53:03 +0200

    Exercice 3 en cours

---
+++ ex03-reactivite-difficile/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+const movie = ref({
+  title: 'Inception',
+  director: 'Christopher Nolan',
+  year: 2010,
+  genre: 'Science-Fiction',
+  duration: 148,
+  rating: 4,
+  synopsis: 'Film sur les rêves lucides',
+})
+</script>
+
+<template></template>
+
+<style scoped></style>
